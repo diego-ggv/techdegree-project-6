73fe38be53c3e951bb1afaab5c0553687eedaa0b
Diego Gutierrez <diego_ggv@pm.me>
2022-07-15 14:52:20 -0400

    update project

---
+++ js/app.js
@@ -0,0 +1,79 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+// const qwerty = document.getElementById('qwerty');
+  const phrase = document.getElementById('phrase');
+  const start = document.querySelector('.start');
+  const ul = phrase.children;
+
+// let missed = 0;
+
+// Start the Game
+  start.addEventListener('click', (e) => {
+    const overlay = document.getElementById('overlay');
+
+    if (e.target.tagName === "BUTTON") {
+      overlay.style.display = "none";
+    }
+  });
+
+
+// Return a random phrase from an array
+  const getRandomPhraseAsArray = (arr) => {
+    const i = Math.floor(Math.random() * 5) ;
+    const array = arr[i];
+    
+    return array.split("");
+  };
+
+  // getRandomPhraseAsArray(phrases);
+  getRandomPhraseAsArray(phrases);
+  console.log(getRandomPhraseAsArray(phrases))
+// Add an array of strings to the display
+// Takes andy array as an argument
+//   const addPhraseToDisplay = (array) => {
+//
+//     // Loop through the array for each character in the array
+//     for (let i = 0; i < array.length; i++) {
+//       // Create a LI element
+//       const li = document.createElement('li');
+//       // Pass array current index value as li text content
+//       li.textContent = array[i];
+//       // Append li to ul
+//       ul.appendChild(li);
+//  
+//       // //if LI text content is strictly equal to a space
+//       // if (li.textContent === ' ') {
+//       //   // LI class equal to "space"
+//       //   li.className = 'space';
+//       // }
+//       // else {
+//       //   // LI class equal to "letter"
+//       //   li.className = 'letter';
+//       // }
+//      
+//     }
+//     // Return newly created LI element
+//     // return ul;
+//   };
+//
+//   const phraseArray = getRandomPhraseAsArray(phrases);
+//   addPhraseToDisplay(phraseArray);
+// console.log(phraseArray);
+
+// Check if the letter is in the phrase
+//   const checkLetter = () => {
+//
+//   };
+
+// check if the game has been won or lost
+//   const checkWin = () => {
+//
+//   };
+
+// Listen for the start game button to be pressed
+
+
+// Listen for the onscreen keyboard to be clicked
+
+
+// });
---
+++ js/phrases.js
@@ -0,0 +1,15 @@
+const phrases = [
+  "I have always imagined that Paradise will be a kind of library",
+  // -by Jorgeluis Borges
+  "It is hard to fail but it is worse never to have tried to succeed",
+  // -by Theodore Roosevelt yes
+
+  "It isn't where you came from. It's where you're going that counts",
+  //-by Ella Fitzgerald yes
+
+  "That which does not kill us makes us stronger",
+  //-by Friedrich Nietzsche yes
+
+  "My mission in life is not merely to survive, but to thrive",
+  //—Maya Angelou
+];
