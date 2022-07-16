a4b3f7dcbbb1387cb7506b44189833e31a731fd9
Diego Gutierrez <diego_ggv@pm.me>
2022-07-16 00:35:02 -0400

    add checkLetter function. The function searches for matching characters in an array, if matching stores the value to a variable  and returns that variable

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // document.addEventListener("DOMContentLoaded", () => {
 
 // const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+// const phrase = document.getElementById('phrase');
 const start = document.querySelector('.start');
-const ul = document.querySelector('#phrase ul')
+const ul = document.querySelector('#phrase ul');
 
 // let missed = 0;
 
@@ -19,21 +19,23 @@ start.addEventListener('click', (e) => {
 
 // Return a random phrase from an array
 const getRandomPhraseAsArray = (arr) => {
+  // Generates a random value taking the length of the given array as max value
   const i = Math.floor(Math.random() * arr.length);
+  // Stores array in a constant taking random number as the array index value
   const array = arr[i];
 
+  // Returns a new created array of characters
   return array.split("");
 };
 
 // Add an array of strings to the display
-// Takes andy array as an argument
 const addPhraseToDisplay = (array) => {
 
 // Loop through the array for each character in the array
   for (let i = 0; i < array.length; i++) {
-//       // Create a LI element
+    // Create a LI element
     const li = document.createElement('li');
-//       // Pass array current index value as li text content
+    // Pass array current index value as li text content
     li.textContent = array[i];
 
     //if LI text content is strictly equal to a space
@@ -45,20 +47,37 @@ const addPhraseToDisplay = (array) => {
       // LI class equal to "letter"
       li.className = 'letter';
     }
+    // Appends LI element to UL node
     ul.appendChild(li);
   }
 
 };
-//
+// Stores generated array in a new constant
 const phraseArray = getRandomPhraseAsArray(phrases);
+// Calls addPhraseToDisplay function and takes phraseArray as parameter
 addPhraseToDisplay(phraseArray);
-console.log(phraseArray)
+console.log(phraseArray);
 
 // Check if the letter is in the phrase
-//   const checkLetter = () => {
-//
-//
-//   };
+const checkLetter = (btn) => {
+  const letterArr = document.getElementsByClassName('letter');
+  let matched = [];
+
+  for (let i = 0; i < letterArr.length; i++) {
+
+    if (letterArr[i].textContent.includes(btn)) {
+      letterArr[i].className = 'show';
+
+      matched.push(letterArr[i]);
+    }
+  }
+
+  console.log(matched);
+  return matched;
+};
+
+
+checkLetter(test);
 
 // check if the game has been won or lost
 //   const checkWin = () => {
